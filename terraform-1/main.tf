8763147681fd9749810566bf7352e68b4cd6da55
igorborodko <igor.borodko@gs-labs.ru>
2023-01-09 21:44:57 +0300

    gitignore+

---
+++ terraform-1/main.tf
@@ -0,0 +1,6 @@
+provider "yandex" {
+  token     = "<AQAAAAAC-neIAATuwcF84NEJVULYtlekoVh1XWU>"
+  cloud_id  = "<b1gi2jp8jll6adpg1frh>"
+  folder_id = "<b1gb2j0k0cdnboead62b>"
+  zone      = "ru-central1-a"
+}
